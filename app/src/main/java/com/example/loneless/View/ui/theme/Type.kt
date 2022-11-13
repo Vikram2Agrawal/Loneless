4355a472109bc8e3fc01b27d1b6d3b9b956d8127
Vikram Agrawal <vmagrawa@usc.edu>
2022-11-13 15:14:20 -0800

    FIX: Renames theming package names to match project name

--- app/src/main/java/com/example/loneless/View/ui/theme/Type.kt
+++ app/src/main/java/com/example/loneless/View/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.csci201_fpp_team_16_code.View.ui.theme
+package com.example.loneless.view.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
